8f83b4c1fd6576b3489237c34e8650eea9abe382
amritlshidling <amritlshidling@gmail.com>
2026-01-05 15:59:53 +0000

    Inventory-service

--- inventory-service/src/main/java/com/zenith/inventory/inventory_service/services/kafka/InventoryConsumer.java
+++ inventory-service/src/main/java/com/zenith/inventory/inventory_service/services/kafka/InventoryConsumer.java
@@ -1,4 +1,28 @@
 package com.zenith.inventory.inventory_service.services.kafka;
 
+import com.zenith.inventory.inventory_service.DTO.ProductCreatedEvent;
+import com.zenith.inventory.inventory_service.entity.Inventory;
+import com.zenith.inventory.inventory_service.mapper.InventoryMapper;
+import com.zenith.inventory.inventory_service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class InventoryConsumer {
+    private final InventoryRepository inventoryRepository;
+    private final InventoryMapper inventoryMapper;
+
+    @KafkaListener(topics = "product-created-events", groupId = "inventory-group")
+    public void consumeProductCreated(ProductCreatedEvent event){
+        log.info("Received event from Kafka: {}", event);
+
+        Inventory inventory = inventoryMapper.fromEventToEntity(event);
+        inventoryRepository.save(inventory);
+        log.info("Initialized inventory for product ID: {}", event.id());
+
+    }
 }
