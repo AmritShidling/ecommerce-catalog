03af1be4d38b46c1a3eba11a7dbeac01b1475204
amritlshidling <amritlshidling@gmail.com>
2026-01-07 17:10:46 +0000

    Service Discovery and API Gateway

---
+++ cloud-gateway/src/main/java/com/zenith/gateway/cloud_gateway/config/GatewayConfig.java
@@ -0,0 +1,33 @@
+package com.zenith.gateway.cloud_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> catalogRoute() {
+        return route("catalog-service")
+                .route(path("/v1/products/**"),http()) // Use empty http()
+                .filter(lb("CATALOG-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoute() {
+        return route("inventory-service")
+                .route(path("/v1/inventory/**"), http())
+                .filter(lb("INVENTORY-SERVICE"))
+                .build();
+    }
+}
--- inventory-service/src/main/java/com/zenith/inventory/inventory_service/services/kafka/InventoryConsumer.java
+++ inventory-service/src/main/java/com/zenith/inventory/inventory_service/services/kafka/InventoryConsumer.java
@@ -4,6 +4,8 @@ import com.zenith.inventory.inventory_service.DTO.ProductCreatedEvent;
 import com.zenith.inventory.inventory_service.entity.Inventory;
 import com.zenith.inventory.inventory_service.mapper.InventoryMapper;
 import com.zenith.inventory.inventory_service.repository.InventoryRepository;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,9 +17,15 @@ import org.springframework.stereotype.Service;
 public class InventoryConsumer {
     private final InventoryRepository inventoryRepository;
     private final InventoryMapper inventoryMapper;
-
+    private final ObservationRegistry observationRegistry;
     @KafkaListener(topics = "product-created-events", groupId = "inventory-group")
     public void consumeProductCreated(ProductCreatedEvent event){
+        Observation observation = observationRegistry.getCurrentObservation();
+        if(observation != null){
+            observation.highCardinalityKeyValue("product.id", String.valueOf(event.id()));
+            observation.highCardinalityKeyValue("product.name", event.name());
+        }
+
         log.info("Received event from Kafka: {}", event);
         log.info("Received event for product: {}. Adjusting stock...", event.name());
         Inventory inventory = inventoryMapper.fromEventToEntity(event);
