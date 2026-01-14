f2c2a4dafc6bb870eaf03abfc574e3381d039d9f
amritlshidling <amritlshidling@gmail.com>
2026-01-14 18:43:52 +0000

    Dockerizing

--- catalog-service/src/main/java/com/zenith/catalog/catalog_service/config/ObservationConfig.java
+++ catalog-service/src/main/java/com/zenith/catalog/catalog_service/config/ObservationConfig.java
@@ -3,6 +3,7 @@ package com.zenith.catalog.catalog_service.config;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +15,13 @@ public class ObservationConfig {
         return new ObservedAspect(observationRegistry);
     }
 
+    @Value("${management.zipkin.tracing.endpoint:http://localhost:9411/api/v2/spans}")
+    private String zipkinEndpoint;
+
     @Bean
     public ZipkinSpanExporter zipkinSpanExporter() {
-        // Points to your Docker Zipkin container
         return ZipkinSpanExporter.builder()
-                .setEndpoint("http://localhost:9411/api/v2/spans")
+                .setEndpoint(zipkinEndpoint) // Uses the injected variable
                 .build();
     }
 
